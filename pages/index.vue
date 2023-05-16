6749bf2540e84ad17382eb15f4da1c219ca0fd06
briofy-mammad <m.ranjbar@briofy.net>
2023-05-16 19:09:52 +0330

    new blog section

--- pages/index.vue
+++ pages/index.vue
@@ -249,11 +249,11 @@
     </div>
   </section>
 
-  <section class="bg-white mb-5 dark:bg-gray-900">
+  <section class="bg-white dark:bg-gray-900 mb-5">
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-      <div class="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
+      <div class="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
         <h2
-          class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white"
+          class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white"
         >
           Our Blog
         </h2>
@@ -262,130 +262,119 @@
           needs of your audience early and often.
         </p>
       </div>
-      <div class="grid gap-8 lg:grid-cols-2">
+      <div class="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
         <article
-          class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+          class="p-4 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
         >
-          <div class="flex justify-between items-center mb-5 text-gray-500">
-            <span
-              class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800"
-            >
-              <svg
-                class="mr-1 w-3 h-3"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"
-                ></path>
-              </svg>
-              Tutorial
-            </span>
-            <span class="text-sm">14 days ago</span>
-          </div>
-          <h2
-            class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+          <a href="#">
+            <img
+              class="mb-5 rounded-lg"
+              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/blog/office-laptops.png"
+              alt="office laptop working"
+            />
+          </a>
+          <span
+            class="bg-purple-100 text-purple-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-purple-200 dark:text-purple-900"
+            >Article</span
           >
-            <a href="#">How to quickly deploy a static website</a>
+          <h2
+            class="my-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+          >
+            <a href="#">Our first office</a>
           </h2>
-          <p class="mb-5 font-light text-gray-500 dark:text-gray-400">
-            Static websites are now used to bootstrap lots of websites and are
-            becoming the basis for a variety of tools that even influence both
-            web designers and developers influence both web designers and
-            developers.
+          <p class="mb-4 font-light text-gray-500 dark:text-gray-400">
+            Over the past year, Volosoft has undergone many changes! After
+            months of preparation and some hard work, we moved to our new
+            office.
           </p>
-          <div class="flex justify-between items-center">
-            <div class="flex items-center space-x-4">
-              <img
-                class="w-7 h-7 rounded-full"
-                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png"
-                alt="Jese Leos avatar"
-              />
-              <span class="font-medium dark:text-white"> Jese Leos </span>
+          <div class="flex items-center space-x-4">
+            <img
+              class="w-10 h-10 rounded-full"
+              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png"
+              alt="Jese Leos avatar"
+            />
+            <div class="font-medium dark:text-white">
+              <div>Jese Leos</div>
+              <div class="text-sm font-normal text-gray-500 dark:text-gray-400">
+                Aug 15, 2021 · 16 min read
+              </div>
             </div>
-            <a
-              href="#"
-              class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline"
-            >
-              Read more
-              <svg
-                class="ml-2 w-4 h-4"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </a>
           </div>
         </article>
         <article
-          class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+          class="p-4 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
         >
-          <div class="flex justify-between items-center mb-5 text-gray-500">
-            <span
-              class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800"
-            >
-              <svg
-                class="mr-1 w-3 h-3"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
-                  clip-rule="evenodd"
-                ></path>
-                <path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path>
-              </svg>
-              Article
-            </span>
-            <span class="text-sm">14 days ago</span>
-          </div>
+          <a href="#">
+            <img
+              class="mb-5 rounded-lg"
+              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/blog/google-hq.png"
+              alt="Google HQ"
+            />
+          </a>
+          <span
+            class="bg-purple-100 text-purple-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-purple-200 dark:text-purple-900"
+            >Article</span
+          >
           <h2
-            class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+            class="my-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+          >
+            <a href="#">We partnered up with Google</a>
+          </h2>
+          <p class="mb-4 font-light text-gray-500 dark:text-gray-400">
+            Over the past year, Volosoft has undergone many changes! After
+            months of preparation and some hard work, we moved to our new
+            office.
+          </p>
+          <div class="flex items-center space-x-4">
+            <img
+              class="w-10 h-10 rounded-full"
+              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/roberta-casas.png"
+              alt="Roberta Casas avatar"
+            />
+            <div class="font-medium dark:text-white">
+              <div>Roberta Casas</div>
+              <div class="text-sm font-normal text-gray-500 dark:text-gray-400">
+                Aug 15, 2021 · 16 min read
+              </div>
+            </div>
+          </div>
+        </article>
+        <article
+          class="p-4 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+        >
+          <a href="#">
+            <img
+              class="mb-5 rounded-lg"
+              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/blog/office-laptops-2.png"
+              alt="office laptops"
+            />
+          </a>
+          <span
+            class="bg-purple-100 text-purple-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-purple-200 dark:text-purple-900"
+            >Article</span
+          >
+          <h2
+            class="my-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
           >
             <a href="#">Our first project with React</a>
           </h2>
-          <p class="mb-5 font-light text-gray-500 dark:text-gray-400">
-            Static websites are now used to bootstrap lots of websites and are
-            becoming the basis for a variety of tools that even influence both
-            web designers and developers influence both web designers and
-            developers.
+          <p class="mb-4 font-light text-gray-500 dark:text-gray-400">
+            Over the past year, Volosoft has undergone many changes! After
+            months of preparation and some hard work, we moved to our new
+            office.
           </p>
-          <div class="flex justify-between items-center">
-            <div class="flex items-center space-x-4">
-              <img
-                class="w-7 h-7 rounded-full"
-                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png"
-                alt="Bonnie Green avatar"
-              />
-              <span class="font-medium dark:text-white"> Bonnie Green </span>
+          <div class="flex items-center space-x-4">
+            <img
+              class="w-10 h-10 rounded-full"
+              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/sofia-mcguire.png"
+              alt="Sofia McGuire avatar"
+            />
+            <div class="font-medium dark:text-white">
+              <div>Sofia McGuire</div>
+              <div class="text-sm font-normal text-gray-500 dark:text-gray-400">
+                Aug 15, 2021 · 16 min read
+              </div>
             </div>
-            <a
-              href="#"
-              class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline"
-            >
-              Read more
-              <svg
-                class="ml-2 w-4 h-4"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </a>
           </div>
         </article>
       </div>
