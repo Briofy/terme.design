fa32d22f4b93da9cb5d4c641a6ea88f690b2c961
briofy-mammad <m.ranjbar@briofy.net>
2023-05-16 17:11:37 +0330

    landing index done

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <section class="text-center dark:text-white">
+  <section
+    class="text-center dark:text-white bg-white mb-5 dark:bg-gray-900 py-10"
+  >
     <h1 class="lg:text-8xl text-4xl font-extrabold">
       Make your website <br />
       <span
@@ -34,7 +36,7 @@
     </div>
   </section>
 
-  <section class="py-20 dark:text-white">
+  <section class="bg-white mb-5 dark:bg-gray-900 py-10 dark:text-white">
     <div class="text-center space-y-5">
       <img
         class="lg:max-w-4xl w-full mx-auto"
@@ -52,7 +54,7 @@
     </div>
   </section>
 
-  <section class="mx-auto dark:text-white">
+  <section class="mx-auto mb-5 dark:text-white">
     <section class="bg-white dark:bg-gray-900">
       <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
         <div class="max-w-screen-md mb-8 lg:mb-16">
@@ -219,7 +221,7 @@
     </section>
   </section>
 
-  <section class="bg-white mt-5 dark:bg-gray-900">
+  <section class="bg-white mb-5 dark:bg-gray-900">
     <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
       <div class="mx-auto max-w-screen-sm text-center">
         <h2
@@ -243,6 +245,217 @@
           class="hidden dark:block w-56 mx-auto"
           alt="qr code"
         />
+      </div>
+    </div>
+  </section>
+
+  <section class="bg-white mb-5 dark:bg-gray-900">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+      <div class="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
+        <h2
+          class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white"
+        >
+          Our Blog
+        </h2>
+        <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">
+          We use an agile approach to test assumptions and connect with the
+          needs of your audience early and often.
+        </p>
+      </div>
+      <div class="grid gap-8 lg:grid-cols-2">
+        <article
+          class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+        >
+          <div class="flex justify-between items-center mb-5 text-gray-500">
+            <span
+              class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800"
+            >
+              <svg
+                class="mr-1 w-3 h-3"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"
+                ></path>
+              </svg>
+              Tutorial
+            </span>
+            <span class="text-sm">14 days ago</span>
+          </div>
+          <h2
+            class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+          >
+            <a href="#">How to quickly deploy a static website</a>
+          </h2>
+          <p class="mb-5 font-light text-gray-500 dark:text-gray-400">
+            Static websites are now used to bootstrap lots of websites and are
+            becoming the basis for a variety of tools that even influence both
+            web designers and developers influence both web designers and
+            developers.
+          </p>
+          <div class="flex justify-between items-center">
+            <div class="flex items-center space-x-4">
+              <img
+                class="w-7 h-7 rounded-full"
+                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png"
+                alt="Jese Leos avatar"
+              />
+              <span class="font-medium dark:text-white"> Jese Leos </span>
+            </div>
+            <a
+              href="#"
+              class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline"
+            >
+              Read more
+              <svg
+                class="ml-2 w-4 h-4"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </a>
+          </div>
+        </article>
+        <article
+          class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+        >
+          <div class="flex justify-between items-center mb-5 text-gray-500">
+            <span
+              class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800"
+            >
+              <svg
+                class="mr-1 w-3 h-3"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
+                  clip-rule="evenodd"
+                ></path>
+                <path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path>
+              </svg>
+              Article
+            </span>
+            <span class="text-sm">14 days ago</span>
+          </div>
+          <h2
+            class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+          >
+            <a href="#">Our first project with React</a>
+          </h2>
+          <p class="mb-5 font-light text-gray-500 dark:text-gray-400">
+            Static websites are now used to bootstrap lots of websites and are
+            becoming the basis for a variety of tools that even influence both
+            web designers and developers influence both web designers and
+            developers.
+          </p>
+          <div class="flex justify-between items-center">
+            <div class="flex items-center space-x-4">
+              <img
+                class="w-7 h-7 rounded-full"
+                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png"
+                alt="Bonnie Green avatar"
+              />
+              <span class="font-medium dark:text-white"> Bonnie Green </span>
+            </div>
+            <a
+              href="#"
+              class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline"
+            >
+              Read more
+              <svg
+                class="ml-2 w-4 h-4"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </a>
+          </div>
+        </article>
+      </div>
+    </div>
+  </section>
+
+  <section class="bg-white dark:bg-gray-900 mb-5">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+      <div
+        class="gap-16 justify-between items-center p-6 text-white bg-blue-600 rounded-lg dark:bg-gray-800 lg:flex lg:gap-24 md:p-12"
+      >
+        <div class="w-full">
+          <h2 class="mb-4 text-3xl tracking-tight font-extrabold sm:text-4xl">
+            Sign up for our newsletter
+          </h2>
+          <p class="font-light sm:text-xl">
+            Stay up to date with the roadmap progress, announcements and
+            exclusive discounts feel free to sign up with your email.
+          </p>
+        </div>
+        <div class="mt-6 w-full lg:mt-0">
+          <form action="#">
+            <div class="items-center mb-3 space-y-4 sm:flex sm:space-y-0">
+              <div class="relative w-full">
+                <label
+                  for="member_email"
+                  class="hidden mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  >Email address</label
+                >
+                <div
+                  class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none"
+                >
+                  <svg
+                    class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"
+                    ></path>
+                    <path
+                      d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"
+                    ></path>
+                  </svg>
+                </div>
+                <input
+                  class="block p-3 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:rounded-none sm:rounded-l-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="Enter your email"
+                  type="email"
+                  required
+                />
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  class="py-3 px-5 w-full text-sm font-medium text-center text-white rounded-lg border cursor-pointer bg-primary-900 border-primary-600 hover:border-primary-700 sm:rounded-none sm:rounded-r-lg hover:bg-primary-700 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Subscribe
+                </button>
+              </div>
+            </div>
+            <div class="text-sm text-left text-gray-400 dark:text-gray-300">
+              We care about the protection of your data.
+              <a href="#" class="font-medium text-white hover:underline"
+                >Read our Privacy Policy</a
+              >.
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   </section>
